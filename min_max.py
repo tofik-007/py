82ae13ca7ca8e44d593eabd7bc3708c76c287549
Taufik Sumara <83862887+tofik-007@users.noreply.github.com>
2022-09-22 17:10:21 +0530

    v_numbers

---
+++ min_max.py
@@ -0,0 +1,5 @@
+a = int(input("enter a first number : "))
+b = int(input("enter a second number : "))
+c = int(input("enter a third number : "))
+print("max(a,b,c) :",max(a,b,c))
+print("min(a,b,c) :",min(a,b,c))
